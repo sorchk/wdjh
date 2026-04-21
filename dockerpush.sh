ac1f717c23dcc771837dc5d23eb3cce2dddeb9da
sorc <sorc@qq.com>
2026-04-21 21:42:03 +0800

    1

---
+++ dockerpush.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+name=multica
+ver=$1
+# 在代码中搜索 my-version，修改版本号
+build_date=$(date +"%Y%m%d")
+if [ -z "${ver}" ]; then
+  ver=0.2.13
+fi
+echo ${ver}_${build_date}
+export DOCKER_CLI_EXPERIMENTAL=enabled
+export DOCKER_BUILDKIT=1
+# docker login -u sorc
+# docker run --privileged --rm tonistiigi/binfmt --install all
+# docker buildx create --name builder --driver=docker-container --use --bootstrap
+# docker buildx ls
+docker buildx build \
+  --platform linux/arm64,linux/amd64 \
+  --build-arg VER=${ver} \
+  --build-arg BUILD_DATE=${build_date} \
+  --push \
+  --tag sorc/${name}:${ver}_${build_date} \
+  --tag sorc/${name}:${ver} \
+  --tag sorc/${name}:latest .
